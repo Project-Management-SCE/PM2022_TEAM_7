06baaae70aa6876f747795baee98827cc6d15f3d
LiorDaichman <liordaichman@gmail.com>
2022-04-24 18:32:48 +0300

    24/04

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home_screen_view(request):
-	context = {}
-	list_of_values = [] 
-	list_of_values.append("first")
-	list_of_values.append("secound")
-	list_of_values.append("third")
-	list_of_values.append("fourth")
-	context['list_of_values'] = list_of_values
-	return render(request, "personal/home.html", context)
+    context = {}
+    return render(request, "personal/home.html")
+
+
+def Sourcesofknowledge_view(request):
+    context = {}
+    return render(request, "personal/Sourcesofknowledge.html")
+
+
+def customersatisfactionsurvey_view(request):
+    context = {}
+    return render(request, "personal/customersatisfactionsurvey.html")
