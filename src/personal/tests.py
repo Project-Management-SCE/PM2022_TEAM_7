81c1172ed0e6b6f0f731a8ee2387ff2fd870b0db
shakedtur <59848826+shakedtur@users.noreply.github.com>
2022-05-08 18:41:55 +0300

    fix unitest

--- src/personal/tests.py
+++ src/personal/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from account.views import registration_view, logout_view, login_view, account_view
 
 
-class Test_account(unittest.TestCase):
+class Test_account(TestCase):
     def create_registration_view(self):
         return account.views.create(email='vikush40@gmail.com', raw_password='medical7')
 
