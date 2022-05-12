71517241881999b2de75b27717f6ace3e6c58ed1
ronbz <ron.bzeve@gmail.com>
2022-05-12 15:41:17 +0300

    added lior and ron

---
+++ src/drugs/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.conf import settings
+from django.shortcuts import reverse
+
+CATEGORY_CHOICES=(
+    ('NS','Non prescription'),
+    ('S','Prescription Needed'),
+)
+
+LABEL_CHOICES=(
+    ('P','primary'),
+    ('S','secondary'),
+    ('D','danger'),
+)
+
+class Item(models.Model):
+    title=models.CharField(max_length=100)
+    price=models.FloatField()
+    discount_price=models.FloatField(blank=True,null=True)
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    label=models.CharField(choices=LABEL_CHOICES,max_length=1)
+    slug=models.SlugField()
+    description=models.TextField()
+    imgurl=models.TextField(max_length=1000)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse("drugs:product", kwargs={'slug': self.slug})
+    def get_add_to_cart_url(self):
+        return reverse("drugs:add-to-cart", kwargs={'slug': self.slug})
+    def get_remove_from_cart_url(self):
+        return reverse("drugs:remove-from-cart", kwargs={'slug': self.slug})
+
+
+class OrderItem(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item=models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity=models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+
+class Order(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items=models.ManyToManyField(OrderItem)
+    start_date=models.DateTimeField(auto_now_add=True)
+    ordered_date=models.DateTimeField()
+    ordered=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
