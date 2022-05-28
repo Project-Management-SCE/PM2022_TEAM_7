07ef5ce8f9aaa4975bef63e2c2dba91fadc16eee
ronbz <34772056+ronbz@users.noreply.github.com>
2022-05-28 20:39:16 +0300

    Update forms.py

--- src/drugs/forms.py
+++ src/drugs/forms.py
@@ -1,15 +1,1 @@
-from django import forms
 
-PAYMENT_CHOICES=(
-    ('S', 'Stripe'),
-    ('P', 'Paypal')
-)
-
-class CheckoutForm(forms.Form):
-    street_address=forms.CharField()
-    apartment_address=forms.CharField(required=False)
-    city_address=forms.CharField()
-    country_address = forms.CharField()
-    zip_address = forms.CharField()
-    save_info=forms.BooleanField(required=False)
-    payment_option=forms.ChoiceField(widget=forms.RadioSelect(),choices=PAYMENT_CHOICES)
