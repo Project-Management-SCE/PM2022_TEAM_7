77426e0ea211fdbfa12f2c284a769047d52c4475
ronbz <ron.bzeve@gmail.com>
2022-05-28 20:38:15 +0300

    Update forms.py

--- src/drugs/forms.py
+++ src/drugs/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class CheckoutForm(forms.Form):
     street_address=forms.CharField()
     apartment_address=forms.charField(required=False)
-    city_address=form.charField()
+    city_address=form.CharField()
     country_address = form.charField()
     zip_address = form.charField()
     save_info=forms.BooleanField(widget=forms.CheckboxInput())
