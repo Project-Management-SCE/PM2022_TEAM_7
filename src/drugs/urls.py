83565f40a8b5805414d3036e04de35fc6696810b
ronbz <ron.bzeve@gmail.com>
2022-05-28 17:11:49 +0300

    BSPM2022T7-185

--- src/drugs/urls.py
+++ src/drugs/urls.py
@@ -6,6 +6,7 @@ from .views import (
     OrderSummaryView,
     add_to_cart,
     remove_from_cart,
+    remove_single_item_from_cart,
 )
 
 
@@ -18,5 +19,5 @@ urlpatterns=[
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
 ]
--- src/drugs/views.py
+++ src/drugs/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,View
 from django.shortcuts import redirect
 from .models import Item, Order, OrderItem
 from django.utils import timezone
@@ -20,9 +21,17 @@ class HomeView(ListView):
     paginate_by=10
     template_name="home.html"
 
-class OrderSummaryView(DetailView):
-    model=Order
-    template_name='order_summary.html'
+class OrderSummaryView(View):
+    def get(self,*args,**kwargs):
+        try:
+            order=Order.objects.get(user=self.request.user, ordered=False)
+            context={
+                'object': order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "you don't have an active order")
+            return redirect("/")
 
 class ItemDetailView(DetailView):
     model=Item
@@ -41,11 +50,11 @@ def add_to_cart(request,slug):
             order_item.quantity+=1
             order_item.save()
             messages.info(request,"This item quantity was updated.")
-            return redirect("drugs:product", slug=slug)
+            return redirect("drugs:order-summary")
         else:
             order.items.add(order_item)
             messages.info(request,"This item was added to your cart.")
-            return redirect("drugs:product", slug=slug)
+            return redirect("drugs:order-summary")
 
     else:
         ordered_date=timezone.now()
@@ -54,7 +63,7 @@ def add_to_cart(request,slug):
             ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect("drugs:product", slug=slug)
+        return redirect("drugs:order-summary")
 
 
 
@@ -67,11 +76,34 @@ def remove_from_cart(request,slug):
             order_item=OrderItem.objects.filter(item=item,
                                                user=request.user,
                                                ordered=False)[0]
-            order_item.quantity -= 1
+            order_item.quantity -= 0
             order_item.save()
             order.items.remove(order_item)
             messages.info(request,"This item was removed from your cart.")
+            return redirect("drugs:order-summary")
+        else:
+            messages.info(request,"This item wasn't in your cart.")
             return redirect("drugs:product", slug=slug)
+    else:
+        messages.info(request, "You don't have an active order")
+        return redirect("drugs:product")
+
+
+def remove_single_item_from_cart(request,slug):
+    item=get_object_or_404(Item,slug=slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item=OrderItem.objects.filter(item=item,
+                                               user=request.user,
+                                               ordered=False)[0]
+            order_item.quantity -= 1
+            if order_item.quantity==0:
+                order.items.remove(order_item)
+            order_item.save()
+            messages.info(request,"This item quantity was updated.")
+            return redirect("drugs:order-summary")
         else:
             messages.info(request,"This item wasn't in your cart.")
             return redirect("drugs:product", slug=slug)
