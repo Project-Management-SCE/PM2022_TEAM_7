9f8805962d0bbc7fd8e0bd8d091dfbd4af3ffa0c
LiorDaichman <73170540+LiorDaichman@users.noreply.github.com>
2022-05-29 15:44:05 +0300

    Update tests.py

--- src/timesheet/tests.py
+++ src/timesheet/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+import timesheet.views
+
 
 # Create your tests here.
+class Test_timesheet(TestCase):
+    def next_year_view(self):
+        return timesheet.views.next_year(1)
