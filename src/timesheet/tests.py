c683244559accc38ca5e039d94eae5eadb52be2f
LiorDaichman <73170540+LiorDaichman@users.noreply.github.com>
2022-05-29 16:17:59 +0300

    Update tests.py

--- src/timesheet/tests.py
+++ src/timesheet/tests.py
@@ -4,5 +4,7 @@ import timesheet.views
 
 # Create your tests here.
 class Test_timesheet(TestCase):
-    def next_year_view(self):
-        self.assertEqual(next_year(1), '2')
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
